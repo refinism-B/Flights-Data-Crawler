f40adcd36c23827723484494b5d81ae4e8614c77
Bevis <add412@gmail.com>
2025-10-10 09:29:51 +0800

    建立git初版，所有程式皆為原本路徑未修改

---
+++ AllCorp_ListCrawler.py
@@ -0,0 +1,177 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date, datetime, timedelta
+import json
+import pandas as pd
+import time
+import os
+from pathlib import Path
+import FlightMod.CrawlList as fcl
+
+def main():
+    flight_corp = ["EVA", "CAL", "SJX", "TTW"]
+
+    for corp in flight_corp:
+        # 判斷總列表.csv檔是否存在，若不存在則先建立一個只有columns的空表格
+        file = f'C:/Users/add41/Documents/Data_Engineer/docker/Develop/FlightCrawler/FlightData/{corp}_FlightList.csv'
+        list_path = Path(file)
+
+        if list_path.exists():
+            pass
+        else:
+            columns = [
+                'query_date',
+                'flight_no',
+                'flight_type',
+                'departure_airport',
+                'departure_airport_code_1',
+                'departure_airport_code_2',
+                'arrival_airport',
+                'arrival_airport_code_1',
+                'arrival_airport_code_2',
+                'link',
+                'sync'
+            ]
+
+            df = pd.DataFrame(columns=columns)
+            df.to_csv(file, index=False)
+
+
+        # 讀入總表檔案
+        df_list = pd.read_csv(file)
+
+
+        # 建立空list（為建立dataframe預備）並設定起始頁數，建立ss連線
+        data = []
+        logtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        start_page = 0
+        ss = requests.Session()
+
+
+        while True:
+            # 網頁為20筆一頁，設定從第0筆開始查詢，每次回圈+20，直到查無資料後break
+            # 利用ss.get發出請求並轉換出soup物件
+            url = f'https://www.flightaware.com/live/fleet/{corp}?;offset={start_page};order=ident;sort=ASC'
+            headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'}
+            res = ss.get(url, headers=headers)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            print(f'開始查詢{corp}的第{start_page}到{start_page + 20}筆資料...')
+
+
+            # 先判斷是否有資料，若找到「查無資料」的標籤，則設 found = True 終止迴圈
+            found = False
+            for tag in soup.find_all('i'):
+                if tag.text == "Sorry. No matching flights found; try again later.":
+                    found = True
+                    break
+
+
+            # 若仍有資料 found = False 則繼續迴圈，分別尋找兩個標籤（兩種皆有連結）並合併list
+            if found == False:
+                table_list = soup('table', class_='prettyTable fullWidth')[0]('tr')[2:]
+
+                for i in table_list:
+                    single = []
+                    flight_no = i('td')[0].span.a.text
+                    print(f'查詢{flight_no}班機資料...')
+
+                    # 紀錄日期
+                    single.append(logtime)
+
+                    # 班機編號
+                    single.append(fcl.safe_extract(lambda: i('td')[0].span.a.text))
+
+                    # 機型
+                    single.append(fcl.safe_extract(lambda: i('td')[1].span.a.text))
+
+                    # 起飛機場
+                    single.append(fcl.safe_extract(lambda: i('td')[2]('span', dir='ltr')[0].text))
+
+                    # 起飛機場代號（如有兩種則分開儲存，只有一種則重複儲存）
+                    code_d = fcl.safe_extract(lambda: i('td')[2]('span', dir='ltr')[1].text)
+                    code_d1, code_d2 = fcl.split_airport_code(code_d)
+                    single.append(code_d1)
+                    single.append(code_d2)
+
+
+                    # 降落機場
+                    single.append(fcl.safe_extract(lambda: i('td')[3]('span', dir='ltr')[0].text))
+
+                    # 降落機場代號（如有兩種則分開儲存，只有一種則重複儲存）
+                    code_a = fcl.safe_extract(lambda: i('td')[3]('span', dir='ltr')[1].text)
+                    code_a1, code_a2 = fcl.split_airport_code(code_a)
+                    single.append(code_a1)
+                    single.append(code_a2)
+
+                    # 連結
+                    single.append('https://www.flightaware.com' + fcl.safe_extract(lambda: i('td')[0].span.a['href']))
+
+                    # 同步標記
+                    single.append(0)
+
+                    data.append(single)
+                    print(f'完成{flight_no}班機資料存取')
+
+
+                # 完成後查詢筆數+20並稍微等待後在進行下一次迴圈
+                print(f'完成存取{corp}的第{start_page}到{start_page + 20}筆資料')
+                start_page += 20
+                time.sleep(15)
+
+
+            # 當查無資料時 found = True 顯示查無資料並終止迴圈
+            else:
+                print(f'{corp}沒有第{start_page}到{start_page + 20}筆資料')
+                break
+
+        print(f'已完成{corp}存取資料')
+
+
+
+        # 根據爬蟲資料建立Dataframe
+        columns = [
+            'query_date',
+            'flight_no',
+            'flight_type',
+            'departure_airport',
+            'departure_airport_code_1',
+            'departure_airport_code_2',
+            'arrival_airport',
+            'arrival_airport_code_1',
+            'arrival_airport_code_2',
+            'link',
+            'sync'
+        ]
+
+        df2 = pd.DataFrame(columns=columns, data=data)
+        print(f'{corp}新資料建檔完成')
+
+
+        # 將機場名稱中的Int'l字樣去除，並去除前後空白
+        df2['departure_airport'] = df2['departure_airport'].str.replace("Int\'l", "").str.replace("Intl", "").str.strip()
+        df2['arrival_airport'] = df2['arrival_airport'].str.replace("Int\'l", "").str.replace("Intl", "").str.strip()
+
+
+        # 直接將新資料與舊資料合併
+        df_combine = pd.concat([df_list, df2], ignore_index=True)
+
+        # 對合併後的資料使用drop_duplicates，將重複值刪去，並覆蓋回df_list
+        df_list = df_combine.drop_duplicates(subset='link', keep = 'first').reset_index(drop=True)
+
+        # 將query_date欄位轉換為datetime物件
+        df_list['query_date'] = pd.to_datetime(df_list['query_date'])
+
+        # 將新的df_list進行存檔
+        df_list.to_csv(file, index=False)
+
+        print(f'完成{corp}資料更新，目前資料筆數：{len(df_list)}')
+        print('5秒後繼續...')
+        time.sleep(5)
+
+    print('已完成所有航空公司資料更新！')
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ FlightMod/FlightInfo.py
@@ -0,0 +1,155 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date, datetime, timedelta
+import json
+import pandas as pd
+import time
+import os
+from pathlib import Path
+
+
+
+
+## 用於航班詳細資訊爬蟲
+
+def trans_date_from_chinese(chinese_date):
+    """將中文日期格式轉為datetime格式"""
+    clean_date = chinese_date.split("(")[0].strip()
+    fmt = "%Y年 %m月 %d日"
+    trans = datetime.strptime(clean_date, fmt)
+
+    return trans
+
+
+
+def find_tag(div_list, target_str:str):
+    """用於尋找特定字串標籤的index"""
+    target=0
+    for i in div_list:
+        if i.get_text() == target_str:
+            break
+        else:
+            target += 1
+    return target
+
+
+
+def safe_extract(func):
+    """判斷一個soup物件是否存在/有值，若沒有則回傳None"""
+    try:
+        return func()
+    except (IndexError, AttributeError):
+        return None
+
+
+
+def gate_exist(soup):
+    """判斷一個航班頁面中是否有到/離閘口資料"""
+    gate = 0
+    x = soup
+    for i in x('div'):
+        if '閘口' in i.text:
+            gate = 1
+            break
+    return gate
+
+
+
+def split_tz(time_str:str):
+    """將帶有時區的時間字串分割，得到[time, timezone]列表"""
+    time_, tz = time_str.split(' ')
+    return time_, tz
+
+
+
+def crawl_flight_data(soup, url, gate_exist:bool):
+    """當該班機有到/離閘門資料時使用的爬蟲"""
+    flight_data = []
+
+    # 班機基本資料。較容易在各網頁中出現差異，故先使用函式取得定位，再去取得資訊
+    div_list = soup('div', class_='flightPageDataLabel')
+
+    # 航班編號、機型、航空公司、飛行距離
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageIdent')[0].h1.text.strip()))
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageDataRow')[find_tag(div_list, '機型')]('div', class_='flightPageData')[0].text.strip().replace('\xa0', ' ')))
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageDataRow')[find_tag(div_list, '航空公司')]('div', class_='flightPageData')[0].text.strip().split('\n')[0]))
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageDataRow')[find_tag(div_list, '距離')].span.text.strip().replace(',', '').split(' ')[1]))
+
+    # 起飛機場、起飛城市
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageSummaryOrigin')[0]('span', class_='displayFlexElementContainer')[0].text.strip()))
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageSummaryOrigin')[0]('span', class_='flightPageSummaryCity')[0].text.strip()))
+
+    # 降落機場、降落城市
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageSummaryDestination')[0]('span', class_='displayFlexElementContainer')[0].text.strip()))
+    flight_data.append(safe_extract(lambda: soup('div', class_='flightPageSummaryDestination')[0]('span', class_='destinationCity')[0].text.strip()))
+
+    # 起飛日期
+    flight_data.append(safe_extract(lambda: soup('span', class_='flightPageSummaryDepartureDay')[0].text))
+    
+    if gate_exist == 1:
+        # 預計/實際離開閘門時間
+        lg_e_time, lg_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[0].span.text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        lg_a_time, lg_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[0]('div', class_="flightPageDataActualTimeText")[0].text.strip().replace('\xa0', ' ').replace('\\n', '').replace('\\t', '')))
+
+        flight_data.append(lg_e_time)
+        flight_data.append(lg_a_time)
+        
+        # 預計/實際起飛時間
+        d_e_time, d_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[1]('span')[1].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        d_a_time, d_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[1]('span')[0].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        
+        flight_data.append(d_e_time)
+        flight_data.append(d_a_time)
+    
+    else:
+        # 預計/實際離開閘門時間
+        flight_data.append(None)
+        flight_data.append(None)
+        
+        # 預計/實際起飛時間
+        d_e_time, d_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[0]('span')[1].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        d_a_time, d_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[0]('span')[0].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        
+        flight_data.append(d_e_time)
+        flight_data.append(d_a_time)
+
+    # 起飛時區
+    flight_data.append(d_a_tz)
+
+    # 抵達日期
+    flight_data.append(safe_extract(lambda: soup('span', class_='flightPageSummaryArrivalDay')[0].text))
+
+    if gate_exist == 1:
+        # 預計/實際降落時間
+        a_e_time, a_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[2]('span')[1].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        a_a_time, a_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[2]('span')[0].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+
+        flight_data.append(a_e_time)
+        flight_data.append(a_a_time)
+
+        # 預計/實際抵達閘門時間
+        ag_e_time, ag_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[3]('span')[1].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        ag_a_time, ag_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[3]('div', class_='flightPageDataActualTimeText')[0].span.text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+
+        flight_data.append(ag_e_time)
+        flight_data.append(ag_a_time)
+    
+    else:
+        # 預計/實際降落時間
+        a_e_time, a_e_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[1]('span')[1].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+        a_a_time, a_a_tz = split_tz(safe_extract(lambda: soup('div', class_='flightPageDataTimesChild')[1]('span')[0].text.strip().replace('\xa0', ' ').replace('\n', '').replace('\t', '')))
+
+        flight_data.append(a_e_time)
+        flight_data.append(a_a_time)
+
+        # 預計/實際抵達閘門時間
+        flight_data.append(None)
+        flight_data.append(None)
+
+    # 降落時區
+    flight_data.append(a_a_tz)
+
+    # 紀錄該航班網址，若有需要可再重新訪問
+    flight_data.append(url)
+
+    return flight_data
