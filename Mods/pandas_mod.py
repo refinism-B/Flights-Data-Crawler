ff2b486644d50de522d77791d9d3f054d0abe265
Bevis <add412@gmail.com>
2025-10-28 08:59:57 +0800

    <modify>：測試並修改列表程式 - 測試並調整航班列表T程式 - 測試並調整機場E程式

--- Mods/pandas_mod.py
+++ Mods/pandas_mod.py
@@ -23,3 +23,29 @@ def exist_or_not(folder, file):
     path = Path(file_path)
 
     return path.exists(), file_path
+
+
+def reassign_id(df, id_col_name, id_str):
+    """根據原有最後一筆資料進行自動編號"""
+
+    # 先找出原本的id編號（id欄位非空）最大值
+    nums = df.loc[df[id_col_name] != "",
+                  id_col_name].str.extract(r"(\d+)").astype(int)
+
+    if nums.empty:
+        start_num = 1
+    else:
+        start_num = nums.max()[0] + 1
+
+    # 計算需要新增的資料數
+    empty_id = df[id_col_name] == ""
+    empty_id_count = empty_id.sum()
+
+    # 先列出編號list
+    new_id = [f"{id_str}{i:03d}" for i in range(
+        start_num, start_num + empty_id_count)]
+
+    # 將list放入df欄位
+    df.loc[empty_id, id_col_name] = new_id
+
+    return df
